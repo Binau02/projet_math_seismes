2c4fe5863fb5a3c88409000d87005b81a83c68aa
Binau02 <aubin.saunier@gmail.com>
2023-06-02 15:27:30 +0200

    add csv

--- Part5.py
+++ Part5.py
@@ -23,12 +23,14 @@ import plotly.express as px
 
 # print(worst_countries)
 
-seismes = pd.read_csv("earthquakes.csv")
-search_max = seismes.copy()
+seismes = pd.read_csv("2014-mag_sup_4.csv")
+
+seismes.drop(seismes[seismes["place"].str.find("Chile") < 0].index, inplace=True)
+# search_max = seismes.copy()
 # print(seismes)
 
-max = search_max.values[search_max["impact.significance"].idxmax()]
-print(max)
+# max = search_max.values[search_max["impact.significance"].idxmax()]
+# print(max)
 
 # seismes.drop(seismes[pd.isna(seismes["pays"])].index, inplace=True)
 # seismes.drop(seismes[pd.isna(seismes["mag"])].index, inplace=True)
@@ -48,7 +50,31 @@ print(max)
 #   search_max.drop("index", axis=1, inplace=True)
 
 # , color="m", color_continuous_scale=palette
-fig = px.scatter_mapbox(seismes, lat="location.latitude", lon="location.longitude", hover_name="location.name", hover_data=["impact.magnitude", "location.depth"], size="impact.significance", size_max=50, opacity=1, zoom=3, height=700, title="là où il faut pas aller en vacances")
+
+# seismes["power"] = 10**(1.5*seismes["mag"]+9.105)
+seismes["power"] = 10**(seismes["mag"])
+
+def get_month(time):
+  return(time[5:7])
+
+seismes["month"] = seismes["time"].apply(get_month)
+# print(seismes)
+
+# print(seismes["month"].value_counts())
+
+
+
+fig = px.scatter_mapbox(seismes, lat="latitude", lon="longitude", hover_name="place", hover_data=["mag", "depth"], size="power", size_max=30, color="month", opacity=1, zoom=3, height=700, title="là où il faut pas aller en vacances")
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
 fig.show()
+
+def get_days(time):
+  return int(time[8:10]) + 30*(int(time[5:7])-1)
+
+seismes["days"] = seismes["time"].apply(get_days)
+# print(seismes["days"].to_numpy())
+
+plt.plot(seismes["days"].to_numpy(), seismes["mag"].to_numpy())
+# plt.plot(seismes["days"].to_numpy(), seismes["power"].to_numpy())
+plt.show()
